b65031b728ce4e4c199b14eab561ce80824ab492
BuildTools <unconfigured@null.spigotmc.org>
2022-10-30 01:07:55 +0900

    一応動く

--- play.py
+++ play.py
@@ -1,2 +1,16 @@
-import requests, json, yaml
+from time import sleep
+import yaml, os
+from discordwebhook import Discord
 
+cfpath = os.path.join(os.getcwd(), "config.yml")
+with open(cfpath) as file:
+    obj = yaml.safe_load(file)
+    discord = Discord(url=obj['URL'])
+
+txts = os.listdir('./txts')
+for txt in txts:
+    with open("txts/{}".format(txt)) as f:
+        content = f.read()
+        # discord.post(content='```' + content + '```')
+        sleep(0.033)
+        print(content)
